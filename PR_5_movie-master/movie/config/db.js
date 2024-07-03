a2eae57bb8215a75914270f924de872d323bc2e7
krunal5291 <krunalkalathiya11@gmail.com>
2024-07-03 09:01:05 +0530

    data

--- PR_5_movie-master/movie/config/db.js
+++ PR_5_movie-master/movie/config/db.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 
 const connection = ()=>{
-    mongoose.connect('mongodb+srv://harshillakhani009:harshillakhani009@cluster0.egpujzu.mongodb.net/?retryWrites=true&w=majority')
+    mongoose.connect('mongodb://localhost:27017')
     console.log('db connected');
 }
 
